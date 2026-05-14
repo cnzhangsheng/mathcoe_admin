b1a16967d84c8e33ca8c00ddf2f2ab5b0a57a513
cnshzs <cnzhangsheng@outlook.com>
2026-05-14 14:39:48 +0800

    更新代码 2026-05-14 14:39

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,8 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
-        changeOrigin: true
+        changeOrigin: true,
+        timeout: 300000
       }
     }
   }
